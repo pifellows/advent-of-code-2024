d0db761d741884b7d2982610b759fedf43b1f5f3
Paul Fellows <paul.i.fellows@gmail.com>
2024-12-10 12:53:02 +0000

    Day 10 part 1

--- day_10/advent.erl
+++ day_10/advent.erl
@@ -6,14 +6,14 @@
 part_1(Filename) ->
     {ok, Content} = file:read_file(Filename),
     {Map, Trailheads, _MaxSize} = parse_map(Content),
+    PeaksFromTrailheads = find_trail_peak_paths(Map, Trailheads),
     UniquePeaksByTrailhead = lists:map(
-                               fun(Trailhead) ->
-                                       PeakEnds = find_trail_peak_paths(Trailhead, Map),
+                               fun(PeakEnds) ->
                                        UniquePeaks = sets:from_list(PeakEnds),
                                        sets:size(UniquePeaks)
                                end,
-                               Trailheads),
-    lists:sum(UniquePeaksByTrailhead).
+                               PeaksFromTrailheads),
+    {UniquePeaksByTrailhead, lists:sum(UniquePeaksByTrailhead)}.
 
 
 parse_map(Content) ->
@@ -32,7 +32,7 @@ generate_map(Lines) ->
     generate_map(Lines, 0, #{}, sets:new()).
 
 
-generate_map([],_LineNumber, Map, Trailheads) ->
+generate_map([], _LineNumber, Map, Trailheads) ->
     {Map, Trailheads};
 generate_map([CurrentLine | Lines], LineNumber, Map, AllTrailheads) ->
     {LineList, Trailheads} = parse_line(CurrentLine),
@@ -40,7 +40,7 @@ generate_map([CurrentLine | Lines], LineNumber, Map, AllTrailheads) ->
     NewTrailheads = sets:union(AllTrailheads, TrailheadsSet),
     NewMap = lists:foldl(
                fun({Y, V}, Acc) ->
-                       maps:put({LineNumber, Y}, V, Acc)
+                       maps:put({LineNumber, Y}, list_to_integer([V]), Acc)
                end,
                Map,
                LineList),
@@ -63,6 +63,48 @@ find_trail_peak_paths(Map, TrailheadSet) ->
     Trailheads = sets:to_list(TrailheadSet),
     lists:map(
       fun(Trailhead) ->
-              0
+              find_peaks(Trailhead, Map)
       end,
       Trailheads).
+
+
+find_peaks(Trailhead, Map) ->
+    Peaks = do_find_peaks(Trailhead, Map),
+    sets:to_list(sets:from_list(lists:flatten(Peaks))).
+
+
+do_find_peaks(Point, Map) ->
+    do_find_peaks(Point, 0, Map, []).
+
+do_find_peaks(Point, Step, Map, Acc) ->
+    case find_next_steps(Point, Step, Map) of
+        {peak, Peak} ->
+            [Peak | Acc];
+        {trail, []} ->
+            Acc;
+        {trail, NextSteps} ->
+            lists:map(
+              fun(NextStep) ->
+                      do_find_peaks(NextStep, Step + 1, Map, Acc)
+              end,
+              NextSteps)
+    end.
+
+
+find_next_steps(Position, 9, _Map) ->
+    {peak, Position};
+find_next_steps(Position, CurrentStep, Map) ->
+    PotentialNextSteps = generate_potential_next_steps(Position),
+
+    {trail, lists:filter(fun(P) -> can_move_to(P, CurrentStep + 1, Map) end, PotentialNextSteps)}.
+
+
+generate_potential_next_steps({X, Y}) ->
+    [{X - 1, Y},
+     {X + 1, Y},
+     {X, Y - 1},
+     {X, Y + 1}].
+
+
+can_move_to(P, DesiredStep, Map) ->
+    maps:get(P, Map, undefined) == DesiredStep.
