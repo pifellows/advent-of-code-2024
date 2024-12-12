25aef8b24c169fc40f675bb4f47d71f05061315d
Paul Fellows <paul.i.fellows@gmail.com>
2024-12-12 17:46:07 +0000

    part 2 partial

--- day_12/advent.erl
+++ day_12/advent.erl
@@ -11,6 +11,20 @@ part_1(Filename) ->
     lists:sum(lists:map(fun({_K, V}) -> V end, Prices)).
 
 
+data(Filename) ->
+    {MaxSize, Points} = from_file(Filename),
+    Nodes = create_edges(Points, MaxSize),
+    partition_graphs(Nodes).
+
+part_2(Filename) ->
+    {MaxSize, Points} = from_file(Filename),
+    Nodes = create_edges(Points, MaxSize),
+    Graphs = partition_graphs(Nodes),
+    Results = lists:map(fun({Tag, Graph}) -> {Tag, calcualte_discount_print(Graph)} end, Graphs),
+    Sum = lists:sum(lists:map(fun({_, Price}) -> Price end, Results)),
+    {Results, Sum}.
+
+
 from_file(Filename) ->
     {ok, Content} = file:read_file(Filename),
     Lines = binary:split(Content, <<"\n">>, [global]),
@@ -23,10 +37,15 @@ get_max_size(Lines) when is_list(Lines) ->
     {length(Lines), size(hd(Lines))}.
 
 
+print_graph(Key, Graphs, Size) ->
+    {Key, Graph} = lists:keyfind(Key, 1, Graphs),
+    Points = maps:map(fun(_K, _V) -> Key end, Graph),
+    print_map(Points, Size).
+
+
 print_map(Points, Size) ->
     Map = compile_map(Points, Size),
     io:format("~s", [Map]).
-
 
 
 compile_map(Points, {MaxX, MaxY}) ->
@@ -89,9 +108,11 @@ test_create_edges({X, Y} = Point, Acc) ->
     {Value, _Edges} = maps:get(Point, Acc),
     NeighboursToCheck = [{X + 1, Y}, {X, Y + 1}],
     ConnectTo = lists:filter(fun(N) -> can_connect(Value, N, Acc) end, NeighboursToCheck),
-    lists:foldl(fun(N, IntAcc) -> 
-        connect(Point, N, IntAcc)
-    end, Acc, ConnectTo).
+    lists:foldl(fun(N, IntAcc) ->
+                        connect(Point, N, IntAcc)
+                end,
+                Acc,
+                ConnectTo).
 
 
 connect(PointA, PointB, Nodes) ->
@@ -108,7 +129,7 @@ can_connect(Value, Point, Nodes) ->
             true;
         _Otherwise ->
             false
-        end.
+    end.
 
 
 connect_nodes(LPoint, {LValue, LEdges1}, RPoint, PointMap) ->
@@ -180,3 +201,141 @@ calculate_fence_price(Graph) ->
     Area = maps:size(Graph),
     Perimeter = maps:fold(fun(_Key, Value, Acc) -> (4 - length(Value)) + Acc end, 0, Graph),
     Area * Perimeter.
+
+
+calcualte_discount_print(Graph) ->
+    Area = maps:size(Graph),
+    Sides = count_sides(Graph),
+    Area * Sides.
+
+
+count_sides(Graph) ->
+    case maps:size(Graph) of
+        0 ->
+            0;
+        1 ->
+            4;
+        2 ->
+            4;
+        _Otherwise ->
+            get_first_perimeter_node(Graph)
+    end.
+
+
+get_first_perimeter_node(Graph) ->
+    Sorted = sort_graph_by_edges(Graph),
+    Start = hd(Sorted),
+    walk_perimeter(Start, Graph).
+
+
+sort_graph_by_edges(Graph) ->
+    lists:sort(fun({_, A}, {_, B}) -> length(A) < length(B) end, maps:to_list(Graph)).
+
+
+walk_perimeter({Point, [Edge]}, Graph) ->
+    %% we have 1 edge. That means we are in a "spur" - therefore there are at least 2 edges we can account for
+    {Px, Py} = Point,
+    {Ex, Ey} = Edge,
+
+    Direction = {Ex - Px, Ey - Py},
+    walk(Point, Edge, Direction, Graph, 2);
+walk_perimeter({Point, [Edge | _]}, Graph) ->
+    {Px, Py} = Point,
+    {Ex, Ey} = Edge,
+
+    InitalDirection = {Ex - Px, Ey - Py},
+    Direction = case can_turn_left(Point, InitalDirection, Graph) of
+                    {true, D} ->
+                        D;
+                    false ->
+                        InitalDirection
+                end,
+    {Dx, Dy} = Direction,
+    Next = {Px + Dx, Py + Dy},
+    walk(Point, Next, {Dx, Dy}, Graph, 1).
+
+
+walk(StartPoint, StartPoint, _Direction, _Graph, Sides) ->
+    Sides;
+walk(StartPoint, CurrentPoint, Direction, Graph, Sides) ->
+    case must_turn(CurrentPoint, Direction, Graph) of
+        {true, NewDirection, NumberOfTurns} ->
+            NextPoint = get_next_point(CurrentPoint, NewDirection),
+            walk(StartPoint, NextPoint, NewDirection, Graph, Sides + NumberOfTurns);
+        false ->
+            NextPoint = get_next_point(CurrentPoint, Direction),
+            walk(StartPoint, NextPoint, Direction, Graph, Sides)
+    end.
+
+
+must_turn(Point, Direction, Graph) ->
+    case can_turn_left(Point, Direction, Graph) of
+        {true, NewDirection} ->
+            {true, NewDirection, 1};
+        false ->
+            case can_go_forward(Point, Direction, Graph) of
+                true ->
+                    false;
+                false ->
+                    case can_turn_right(Point, Direction, Graph) of
+                        {true, NewDirection} ->
+                            {true, NewDirection, 1};
+                        false ->
+                            {true, reverse(Direction), 2}
+                    end
+            end
+    end.
+
+
+can_turn_left({Px, Py} = _Point, Direction, Graph) ->
+    LeftDirection = turn_left(Direction),
+    {Lx, Ly} = LeftDirection,
+    case maps:is_key({Px + Lx, Py + Ly}, Graph) of
+        true ->
+            {true, LeftDirection};
+        false ->
+            false
+    end.
+
+
+turn_left({1, 0}) ->
+    {0, 1};
+turn_left({0, 1}) ->
+    {-1, 0};
+turn_left({-1, 0}) ->
+    {0, -1};
+turn_left({0, -1}) ->
+    {1, 0}.
+
+
+can_turn_right({Px, Py} = _Point, Direction, Graph) ->
+    RightDirection = turn_right(Direction),
+    {Rx, Ry} = RightDirection,
+    case maps:is_key({Px + Rx, Py + Ry}, Graph) of
+        true ->
+            {true, RightDirection};
+        false ->
+            false
+    end.
+
+
+turn_right({1, 0}) ->
+    {0, -1};
+turn_right({0, 1}) ->
+    {1, 0};
+turn_right({-1, 0}) ->
+    {0, 1};
+turn_right({0, -1}) ->
+    {-1, 0}.
+
+
+can_go_forward({Px, Py}, {Dx, Dy}, Graph) ->
+    maps:is_key({Px + Dx, Py + Dy}, Graph).
+
+
+reverse({X, Y}) ->
+    {-X, -Y}.
+
+
+get_next_point({Px, Py} = _Point, {X, Y} = _Direction) ->
+    {Px + X, Py + Y}.
